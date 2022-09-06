5c2765fe8b1d6f073b06bc48bf752f0f1cb3a910
jerryjohns <jerry.johns@student.moringaschool.com>
2022-09-06 21:27:10 +0300

    commit tree 8ad719378354087c03325beb2d9fb31d8b9eb8fe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
@@ -15,5 +16,29 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+  post ("/reviews") do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    );
+    review.to_json
+ end
+ patch ("/reviews/:id") do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+ end
+
+ delete ("/reviews/:id") do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
 
 end
